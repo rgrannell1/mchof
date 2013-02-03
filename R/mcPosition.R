a07aeff06a205b98fb3016307335c8cfbe4919b9
r.grannell1 <r.grannell1@nuigalway.ie>
2013-02-03 23:28:04 +0000

    significant contributions towards mcPosition

--- R/mcPosition.R
+++ R/mcPosition.R
@@ -8,11 +8,22 @@ mcPosition <- function(f, x, right, nomatch, paropts = NULL){
 	ind <- matrix(NA, 
 		nrow = ncores,
 		ncol = ceiling(length(x)/ncores))
-	ind[seq_along(x)] <- x
-
-	for(){
+	ind[seq_along(x)] <- seq_along(x)
+	
+	for(i in if(right) ncores:1 else 1:ncores){
 		
+		res <- Reduce(
+			f = function(e1, e2) rbind(e1, e2), 
+			x = call_mclapply(
+	   				function(j) c(j, x[[j]]), 
+	   				ind[i,], paropts) )
+		res <- res[,1] * res[,2]
+		
+		if(any(res != 0)){
+			return((ind[i,])[min(res > 0)]
+		}
 	}
+	nomatch
 }
 
 
