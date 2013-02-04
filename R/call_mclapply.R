ad63a941d9d717ac12f3b244372b0b4a1d27a81a
r.grannell1 <r.grannell1@nuigalway.ie>
2013-02-04 23:22:55 +0000

    inproved error checks in call_mclapply

--- R/call_mclapply.R
+++ R/call_mclapply.R
@@ -1,39 +1,37 @@
 #
 #
 
-call_mclapply <- function(f, x, paropts = NULL){
+call_mclapply <- function(f, x, ordered = NULL, paropts = NULL){
 	# provides the interface to the parallel backend for 
 	# other functions in mchof
 
 	arg_names <- names(paropts)
 	invalid_args <- setdiff(arg_names, formals(mclapply))
 	
-	if(length(invalid_args) > 0){
-		
-		stop(paste(
-			invalid_args,
-			"are not formal arguments for mclapply"))
-		
+	if(length(invalid_args) > 0){	
+		stop(invalid_args, " are not formal arguments for mclapply")
 	}
-
+	if(any(c('f', 'FUN') %in% paropts)){
+		stop('f or FUN may not be specified in paropts')
+	}
+	if(any(c('x', 'X') %in% paropts)){
+		stop('x or X may not be specified in paropts')
+	}
+	
+	
 	do.call(mclapply, c(list(FUN = f, X = x), paropts))
 }
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 #-#-# UNIT TESTS FOR CALL_MCLAPPLY
 #  
-# 
+# likely bug: order not preserved.
 # 
 #-----------------------------#
 
 require(testthat); require(multicore)
 
-replicate(
-	100,
-	
-)
-
-
+call_mclapply()
 
 
 
