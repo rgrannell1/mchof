45ee8174f0aa49e7584022903093468de95a38f3
r.grannell1 <r.grannell1@nuigalway.ie>
2013-02-04 00:09:13 +0000

    fixed bug in call_mclapply, worked on mcPosition

--- R/call_mclapply.R
+++ R/call_mclapply.R
@@ -7,9 +7,9 @@
 call_mclapply <- function(f, x, paropts = NULL){
 	# provides the parallel backend for other functions in mchof
 	
-	if(TRUE){
+	if(!TRUE){
 		stop('invalid formal arguments passed to mclapply')	
 	}
 	
-	do.call(f, c(list(FUN = f, X = x), paropts))
+	do.call(mclapply, c(list(FUN = f, X = x), paropts))
 }
--- R/mcPosition.R
+++ R/mcPosition.R
@@ -1,5 +1,5 @@
 
-mcPosition <- function(f, x, right, nomatch, paropts = NULL){
+mcPosition <- function(f, x, right=FALSE, nomatch, paropts = NULL){
 	
 	ncores <- if(!is.null(paropts) && 'mc.cores' %in% names(paropts)){
 		paropts$mc.cores
@@ -9,22 +9,35 @@ mcPosition <- function(f, x, right, nomatch, paropts = NULL){
 		nrow = ncores,
 		ncol = ceiling(length(x)/ncores))
 	ind[seq_along(x)] <- seq_along(x)
+	ind <- t(ind)
 	
-	for(i in if(right) ncores:1 else 1:ncores){
+	print(ind)
+	
+	for(i in if(right) nrow(ind):1 else 1:nrow(ind)){
 		
 		res <- Reduce(
-			f = function(e1, e2) rbind(e1, e2), 
+			f = rbind, 
 			x = call_mclapply(
-	   				function(j) c(j, x[[j]]), 
+	   				function(j) c(j, f(x[[j]])), 
 	   				ind[i,], paropts) )
-		res <- res[,1] * res[,2]
 		
-		if(any(res != 0)){
-			return((ind[i,])[min(res > 0)]
+		print(res)
+		
+		res <- res[,1] * res[,2]
+
+		if(res != 0){
+			return((ind[i,])[min(res > 0)])
 		}
 	}
 	nomatch
 }
+
+mcPosition(function(x) x > 5, 1:10, paropts = list(mc.cores = 2))
+
+
+
+
+
 
 
 
