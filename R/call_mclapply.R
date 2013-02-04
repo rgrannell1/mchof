0bd24c848b731108150e399a60ef67b3a5a0609a
r.grannell1 <r.grannell1@nuigalway.ie>
2013-02-04 04:22:13 +0000

    updated call_mclapply

--- R/call_mclapply.R
+++ R/call_mclapply.R
@@ -2,11 +2,18 @@
 #' 
 
 call_mclapply <- function(f, x, paropts = NULL){
-	# provides the parallel backend for other functions in mchof
+	# provides the interface to the parallel backend for 
+	# other functions in mchof
 	
-	if(!TRUE){
-		stop('invalid formal arguments passed to mclapply')	
+	invalid_args <- setdiff(names(paropts), formals(mclapply))
+	
+	if(length(invalid_args) > 0){
+		
+		stop(paste(
+			invalid_args,
+			"are not formal arguments for mclapply"))
 	}
-	
+	#'#' Multiple FUN, f, x, X, ..., arguments
+
 	do.call(mclapply, c(list(FUN = f, X = x), paropts))
 }
