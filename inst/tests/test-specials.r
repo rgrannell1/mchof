3a1f6aa438bd3b9148fb2b90f0a4d45516a09063
r.grannell1 <r.grannell1@nuigalway.ie>
2013-02-14 05:38:17 +0000

    added part of mcPosition's unit test

--- inst/tests/test-specials.r
+++ inst/tests/test-specials.r
@@ -3,16 +3,54 @@ context("make sure that the special cases for mchof are sensible")
 
 test_that("mcFilter's special cases work like x[which(...)]", {
 	
+	x <- c(NA, NA)
 	expect_equal(
-		mcFilter(function(x) is.na(x), c(NA, NA)),
+		mcFilter(function(x) is.na(x), x),
 		x[which(is.na(x))])
 	
 	expect_equal(
-		mcFilter(function(x) !is.na(x), c(NA, NA)),
+		mcFilter(function(x) !is.na(x), x),
 		x[which(!is.na(x))])
 	
+	x <- c(NULL, NULL)
 	expect_equal(
 		mcFilter(
-			function(x) is.null(x), NULL),
-		NULL)
+			function(x) is.null(x), x,
+		), x[which(is.null(x))]
+		
+	x <- integer(0)
+	expect_equal(
+		mcFilter(
+			function(x) T, x,
+		), x)
 })
+
+test_that("mcPosition's special cases work like 
+	x[which(...)[1]]", {
+	
+	x <- c(1,3,2,4,10)
+	expect_equal(
+		mcPosition(
+			function(x) x > 3,	
+			x),		
+		x[which(x > 3)[1]])
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
