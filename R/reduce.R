821095256db0594909f4c2baede93054f0322b96
r.grannell1 <r.grannell1@nuigalway.ie>
2013-02-20 22:07:48 +0000

    mcReduce times correctly. Remove missing value NA in call_mclapply; use a custom missing type instead

--- R/reduce.R
+++ R/reduce.R
@@ -4,7 +4,8 @@
 #' @export
 #' @description \code{mcReduce} can be used as a parallel replacement for a 
 #' subclass of problems that can be solved with \code{Reduce} ; parallelising 
-#' \code{Reduce}  is only possible when the function f is associative; that is
+#' \code{Reduce} is only possible when the function f is associative; in infix 
+#' notation that is
 #' 
 #' \code{(x1 f x2) f x3} is equivalent to \code{x1 f (x2 f x3)}. In practicality
 #' this means that the order in which the Reduce is carried out isn't important,
@@ -17,7 +18,7 @@
 #'   
 #' @name mcReduce
 #' 
-#' @param f a binary function
+#' @param f a two-argument function
 #' @param x a vector or list
 #' @param paropts paropts a list of parameters to be handed to 
 #'    mclapply (see details and \code{\link{mclapply}})
@@ -26,9 +27,9 @@ mcReduce <- function(f, x, paropts = NULL){
 	# multicore version of Reduce
 	
 	to_pairs <- function(flatlist){
-		# takes a list of elements x in X, returns a 
-		# list of lists, each containing two elements
-		# from X, or one in the last nested list if x has odd length
+		# takes a list [x1, ..., xn], returns a list of pairs
+		# [ [x1, x2], ..., [x(n-1), xn | NA] ]. If flatlist is odd length 
+		# the last element in the last list is NA
 			
 		Map(function(i){
 				
@@ -47,17 +48,21 @@ mcReduce <- function(f, x, paropts = NULL){
 	# successively reduce pairs of elements in not_reduce to a single element
 	while(length(not_reduced) > 1){
 
-		not_reduced <- to_pairs(call_mclapply(
-			function(val_pair){
-				# returns f(x1, x2), or x1 
-				
-				if(is.na(val_pair$second)){
-					val_pair$first
-				} else {
-					f(val_pair$first, val_pair$second)
-				}	
-			},	
-			x = not_reduced, paropts))
+		not_reduced <- to_pairs(
+			call_mclapply(
+				function(val_pair){
+					# returns f(x1, x2), or x1 if only one 
+					
+					# WHY IS NA SPECIAL?
+					
+					
+					if(is.na(val_pair$second)){
+						val_pair$first
+					} else {
+						f(val_pair$first, val_pair$second)
+					}	
+				},	
+				x = not_reduced, paropts))
 	}
 	
 	f(not_reduced[[1]]$first, not_reduced[[1]]$second) 
