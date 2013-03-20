b2d9f25a5f5792d4e8f4e905b08ced2e67cbf3ed
r.grannell1 <r.grannell1@nuigalway.ie>
2013-03-20 12:27:38 +0000

    copied code from Zip |-> unZip; need to actually make it an inverse of Zip. Test with %of%

--- R/unzip.R
+++ R/unzip.R
@@ -17,7 +17,36 @@
 #' @keywords mcUnzip
  
 mcUnzip <- function (..., paropts) {
-	# inverse of mcZip: mcUnzip ( mcZip (x) ) |-> x 
+	# rough inverse of mcZip: mcUnzip ( mcZip (x) ) |-> x 
 	
+	# list ( list(x1, y1), list(x2, y2) ) |-> list ( list (x1, x2), list (y1, y2) )
+	
+	args <- lapply (as.list(match.call())[-1], eval)
+	
+	if (length(args) == 0) return (NULL)
+	
+	if (!is.null(names(args))) {
+		
+		if ('paropts' %in% names(args)) args$paropts <- NULL
+		if ('f' %in% names(args)) args$f <- NULL
+		
+	}
+	
+	shortest <- min(sapply(args, length))	
+	
+	# PATCH ME UP !!
+	
+	to_unzip <- lapply (
+		args, function (x) x[seq_len(shortest)] )
+	types <- lapply (
+		args,	
+		function (x) 1 )  
+	
+	call_mclapply (
+		f = function (ind) {
+			lapply (to_zip, function (x) x[[ind]])
+		},	
+		x = seq_len(shortest), 
+		paropts)
 	
 }
