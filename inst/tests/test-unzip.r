1fe5f882c45b72de63846d72b2646c62cee6506f
r.grannell1 <r.grannell1@nuigalway.ie>
2013-04-02 00:17:58 +0100

    worked on mcUnzip test

--- inst/tests/test-unzip.r
+++ inst/tests/test-unzip.r
@@ -2,27 +2,22 @@
 context ("test that mcUnzip is mostly inverse to mcZip...mostly")
 
 test_that ("verify that is a rough inverse of mcZip", {
-		
-	zip_identity <- function (..., paropts) {		
-		mcUnzip (mcZip (..., paropts))
-	}
 	
-	expect_equal (
-		zip_identity (
-			list (1, 2, 3),	
-			list (4, 5, 6) ),	
-		list (
-			list (1, 2, 3),	
-			list (4, 5, 6) ) )
+	'%of%' <- function (f, g) {
+		function (...) f(g(...))
+	}
+	cancelled <- mcUnzip %of% mcZip
 	
 	expect_equal(
-		zip_identity (
-			
-			
-			
-		),
-		
-	)
+		cancelled( list(
+			list('a', 1),		
+			list('b', 2),
+			list('c', 3)
+		)),
+	list (
+		list('a', 'b', 'c'),
+		list(1, 2, 3)
+	))
 	
 })
 
