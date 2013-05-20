ae72afc6d77822ad690c045a44c3a12264690860
r.grannell1 <r.grannell1@nuigalway.ie>
2013-05-20 21:50:30 +0100

    added windows-specific tests that will only be run on a windows OS

--- inst/tests/test-backend.r
+++ inst/tests/test-backend.r
@@ -23,25 +23,7 @@ if (exists('call_mclapply')) {
 		})
 		
 	})
-	
-	test_that("error when given bad arguments", {
-	    expect_error(
-	    	call_mclapply("cat", 1:10, list(mc.cores = 2)),
-	    	regexp = "f is not")
-	    
-	    expect_error(
-	    	call_mclapply(function(x) x, factor(1), list(mc.cores = 2)),
-	    	regexp = "x is not")
-	    
-	    expect_error(
-	    	call_mclapply(function(x) x, 1:10, list(FUN = identity)),
-	    	regexp = "FUN may not")
-	    
-	    expect_error(
-	    	call_mclapply(function(x) x, 1:10, list(monkey = identity)),
-	    	regexp = "invalid")
-	})
-	
+
 	test_that("returns correct results for sample cases", {
 		expect_equal(
 			call_mclapply(function(x) x, 1:5),
@@ -49,26 +31,47 @@ if (exists('call_mclapply')) {
 		
 		expect_equal(
 			call_mclapply(function(x) !is.na(x),
-			c(1, 2, 3, NA, 4, 5)), list(TRUE, TRUE, TRUE, FALSE, TRUE, TRUE))
-	
-	})
-	
-	test_that("check that everything's running quick", {
-		
-		expect_that(
-			system.time(call_mclapply(
-				function(x) Sys.sleep(0.5), 1:10, list(mc.cores = 2)))[3],	
-			takes_less_than(3)) # at least 60% speedup
-	
-	})
-	
-	test_that("check that the options mechanism works for parallel backend", {
-		options(mc.cores = 2)
-		
-		expect_that(
-			system.time(call_mclapply(
-				function(x) Sys.sleep(0.5), 1:10))[3],	
-			takes_less_than(3)) # at least 60% speedup
+						  c(1, 2, 3, NA, 4, 5)), list(TRUE, TRUE, TRUE, FALSE, TRUE, TRUE))
 		
 	})
+	
+	if (.Platform$OS.type == 'unix') {
+		
+		test_that("error when given bad arguments", {
+		    expect_error(
+		    	call_mclapply("cat", 1:10, list(mc.cores = 2)),
+		    	regexp = "f is not")
+		    
+		    expect_error(
+		    	call_mclapply(function(x) x, factor(1), list(mc.cores = 2)),
+		    	regexp = "x is not")
+		    
+		    expect_error(
+		    	call_mclapply(function(x) x, 1:10, list(FUN = identity)),
+		    	regexp = "FUN may not")
+		    
+		    expect_error(
+		    	call_mclapply(function(x) x, 1:10, list(monkey = identity)),
+		    	regexp = "invalid")
+		})
+	
+		test_that("check that everything's running quick", {
+			
+			expect_that(
+				system.time(call_mclapply(
+					function(x) Sys.sleep(0.5), 1:10, list(mc.cores = 2)))[3],	
+				takes_less_than(3)) # at least 60% speedup
+		
+		})
+		
+		test_that("check that the options mechanism works for parallel backend", {
+			options(mc.cores = 2)
+			
+			expect_that(
+				system.time(call_mclapply(
+					function(x) Sys.sleep(0.5), 1:10))[3],	
+				takes_less_than(3)) # at least 60% speedup
+			
+		})
+	}
 }
