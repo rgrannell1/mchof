ba6c4e817decf52c4c6a57e714f1c7b15b2c390c
r.grannell1 <r.grannell1@nuigalway.ie>
2013-05-20 02:19:50 +0100

    mcUnfold added and partly tested. Folds still need length0 tests

--- R/unfold.R
+++ R/unfold.R
@@ -3,7 +3,8 @@
 #' @export
 #' @description mcUnfold
 #' 
-#' @details mcUnfold is dual to mcFold. 
+#' @details mcUnfold is potentially non-terminating; caution must be taken to
+#' terminate the unfold.
 #' 
 #' @name mcUnfold
 #' 
@@ -12,31 +13,48 @@
 #' @param paropts paropts a list of parameters to be handed to 
 #'    mclapply (see details and \code{\link{mclapply}})
 #' 
-
-# ana f x = case (f x) of
-# 	Nothing -> []
-# 	Just (a,y) ->  a:(ana f y)
+#' @example 
+#' 
+#' # enumerate the integers 1...10
+#' mcUnfold (
+#'     function (x) if (x < 10) c(x, x+1),
+#'     list(1)
+#' )
+#' 
+#' 
 
 mcUnfold <- function (f, x, paropts = NULL) {
-	# list anamorphism. from an initial seed value, producing
+	# list anamorphism. from an initial seed value, produce
 	# a potentially infinite amount of values. 
+	# may be non-terminating
+
+	is_empty_list <- function (x) {
+		is.null(x) ||
+			(is.list(x) && length(x) == 0)
+	}
 	
 	f <- match.fun(f)
-			
 	
+	if (is.null(x)) return (NULL)
+	if (is.list(x) && length(x) == 0) return(list())
+	if (is.factor(x)) stop('x may not be a factor')	
 	
-	
+	unfoldable <- c(x)
 
+	len <- function () {
+		length(unfoldable)
+	}
+
+	repeat {
+		res <- f( unfoldable[[ len() ]] )
+		
+		if (is_empty_list(res)) {
+			return (unfoldable)
+		} else {
+			unfoldable <- c(head(unfoldable, -1), res)
+		}
+	}
+	
+	unfoldable
 }
-
-
-
-
-
-
-
-
-
-
-
 
--- inst/tests/test-unfold.r
+++ inst/tests/test-unfold.r
@@ -3,16 +3,23 @@ context("unfold is well behaved")
 
 test_that("normal cases of mcUnfold work", {
 	
-	mcUnfold(
-		f = function (val) {
-			list(val, val + 1)		
-		},
-		x = 0
-	)
+	expect_equal(
+		mcUnfold(
+			f = function (n) {
+				if (n < 4) list(n, n + 1)		
+			},
+			x = list(0)
+		),
+		list(1,2,3,4,5))
 	
-	
-	
-	
-	
-	
+	expect_equal(
+		mcUnfold(
+			f = function (n) {
+				if (n < 32) list(n, 2 * n)		
+			},
+			x = c(2),
+			paropts = list(mc.cores = 2)
+		),
+		list(2,4,8,16,32))
+
 })
