6b8c893fd66a824e85d58e4e83fe9f9f69fd0429
r.grannell1 <r.grannell1@nuigalway.ie>
2013-05-26 22:48:03 +0100

    worked on factor error code

--- inst/tests/test-factors.r
+++ inst/tests/test-factors.r
@@ -9,58 +9,39 @@ test_that("errors thrown by all functions", {
 	true_fun <- function(...) TRUE
 	test_factor <- factor(c(1,2,3,2,1))
 	
-	expect_error(
-		mcReduce(true_fun, test_factor), 'factor')
+	expect_error(mcReduce(true_fun, test_factor), 'factor')
 	
-	expect_error(
-		mcFind(true_fun, test_factor), 'factor')
+	expect_error(mcFind(true_fun, test_factor), 'factor')
 	
-	expect_error(
-		mcPosition(true_fun, test_factor), 'factor')
+	expect_error(mcPosition(true_fun, test_factor), 'factor')
 	
-	expect_error(
-		mcFilter(true_fun, test_factor), 'factor')	
+	expect_error(mcFilter(true_fun, test_factor), 'factor')	
 	
 	### mchof 0.2 functions ###
 	#-#-#-#-#-#-#-#-#-#-#-#-#-#
 	
-	expect_error(
-		mcZipWith(
-			function (...) TRUE,
-			list(factor(1:10), factor(11:20))
-		), 'factor')
+	expect_error(mcZipWith(truefun, list(test_factor, test_factor)), 'factor')
 	
-	expect_error(
-		mcZip(
-			list(factor(1:4), factor(5:8))
-		), 'factor')
+	expect_error(mcZip(list(test_factor, test_factor)), 'factor')
 	
-	expect_error(
-		mcUnzip(
-			list(factor(1:5))	
-		), 'factor')
+	expect_error(mcUnzip(list(test_factor)), 'factor')
 	
-	expect_error(
-		mcUnzipWith(
-			list(factor(1:5))	
-		), 'factor')
+	expect_error(mcUnzipWith(truefun, list(test_factor)), 'factor')
 	
-	expect_error(
-		mcPartition(true_fun, test_factor),	'factor')		
+	expect_error(mcPartition(true_fun, test_factor), 'factor')		
 	
 	### mchof 0.3 functions ###
 	#-#-#-#-#-#-#-#-#-#-#-#-#-#
 	
-	expect_error(
-		mcAll(true_fun, test_factor), 'factor'	
-	)
+	expect_error(mcAll(true_fun, test_factor), 'factor')
 	
-	expect_error(
-		mcAny(true_fun, test_factor), 'factor'	
-	)
+	expect_error(mcAny(true_fun, test_factor), 'factor')
 
-	expect_error(
-		mcOne(true_fun, test_factor), 'factor'	
-	)
+	expect_error(mcOne(true_fun, test_factor), 'factor')
 	
-})
+	expect_error(mcReject(true_fun, test_factor), 'factor')
+
+	expect_error(mcFold(true_fun, 0, test_factor), 'factor')	
+	
+}
+		  
