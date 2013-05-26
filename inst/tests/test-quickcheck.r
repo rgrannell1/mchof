52fecfbdcebcc303c6e3b765229c759b25d0b7ee
r.grannell1 <r.grannell1@nuigalway.ie>
2013-05-26 02:24:10 +0100

    worked on generator function. Very inelegant

--- inst/tests/test-quickcheck.r
+++ inst/tests/test-quickcheck.r
@@ -4,7 +4,7 @@ context('quickcheck functions where possible')
 test_that('', {
 
 	forall(
-		list(x_ = lists_with_null(), paropts_ = random_paropts),
+		list(x_ = rInputs$, paropts_ = rInputs$paropts),
 		function (x_, paropts_) {
 			res_1 <- mcFilter(Negate(is.null), x_, paropts_)
 			res_2 <- denullify(x_)
--- tests/test_utils.R
+++ tests/test_utils.R
@@ -8,6 +8,9 @@ pick_size <- function (n) {
 rep_sample <- function (x, n) {
 	sample(x, size = n, replace = TRUE)
 }
+repl <- function (n, expr) {
+	replicate(n, expr, FALSE)
+}
 
 # so. many. magic. numbers !!
 
@@ -22,8 +25,7 @@ rInputs <- list(
 		rep_sample(letters[1:26], n)	
 	},
 	strings = function (n) {
-		
-		replicate(n, ( function () {		
+		repl(n, ( function () {		
 			list( paste0(rInputs$letters( pick_size(20) ), collapse = '') )
 		} )())
 	},
@@ -34,40 +36,100 @@ rInputs <- list(
 		rep_sample(c(TRUE, FALSE, NA),  n)
 	},
 	null = function (n) {
-		rep_sample(list(NULL), n)	
+		NULL
 	},
 	
 	character_vectors = function (n) {
-		replicate(n, list( rInputs$strings( pick_size(20) ) ))
+		repl(n, list( rInputs$strings( pick_size(20) ) ))
 	},
 	integer_vectors = function (n) {
-		replicate(n, list( rInputs$integers( pick_size(20) ) ))
+		repl(n, list( rInputs$integers( pick_size(20) ) ))
 	},
 	float_vectors = function (n) {
-		replicate(n, list( rInputs$floats( pick_size(20) ) ))
+		repl(n, list( rInputs$floats( pick_size(20) ) ))
 	},
-	
-	flatlist = function (n) {
-		replicate(n, {
-	
-			funcs <- list(
-				rInputs$integers,
+
+	list_cells = function (n) {
+		repl(n, {
+			func <- pick_one(list(
+				rInputs$integers, rInputs$floats, 
 				rInputs$letters,
-				rInputs$strings,
-				rInputs$booleans,
-				rInputs$logicals,
-				rInputs$character_vectors,
-				rInputs$integer_vectors,
-				rInputs$logicals
-			)
-			test_fun <- pick_one(funcs)[[1]]
-			
-			list( test_fun(pick_size(20)) )
+				rInputs$booleans, rInputs$logicals,
+				rInputs$null))[[1]]
+							 
+			list ( func(pick_size(20)) )
 		})
 	},
-	flatlist_with_null = function (n) {
-		replicate(n, {
-				
+	flatlist = function (n) {
+		repl(n, rInputs$list_cells( pick_size(20) ))
+	},
+	paropts = function (n) {
+		repl(n, {
+			cores <- pick_size(5)
+			list(mc.cores = cores)
 		})
 	}
 )
+
+# a placeholder testing function, until I write something 
+# better. Used to test properties & lack of errors. Not very elegant, so
+# I don't recommend that you use this code yourself. 
+
+forall <- function (
+	cases, expect, given = function (...) TRUE,
+	opts = list(time = 2), info = '') {
+	# a lightweight quickcheck function
+	
+	stopwatch <- function (seconds) {
+		# returns a function with Sys.time( ) 
+		# captured in a closure
+		
+		stopifnot (is.numeric(seconds) && seconds > 0)
+		
+		( function () {
+			start_time <- Sys.time()
+			function () {
+				time_passed <- as.numeric(difftime( Sys.time(), start_time ))
+				time_passed < seconds
+			}		
+		} )()
+	}
+	
+	testargs_iter <- ihasNext(do.call(product, cases))
+	
+	predicate <- function (args) {
+		if (do.call(given, args)) do.call(expect, args) else TRUE
+	}
+	
+	results <- c()
+	time_left <- stopwatch(opts$time)
+	
+	while (hasNext(testargs_iter) && time_left()) {
+		
+		args <- nextElem(testargs_iter)
+		test_return_value <- predicate(args)
+		
+		if (!is_boolean(test_return_value)) {
+			stop(args, " didn't return t/f: actual value was ", test_return_value)
+		}
+		results <- c(
+			results,
+			list(list(
+				passed = test_return_value,
+				args = args
+		)))
+	}
+	
+	Map(
+		function (test) {			
+			(!test$passed) %throws% stopf(
+				c(
+					'failed! %s',
+					"the assertion wasn't true when %s were equal to %s"),
+				info, 
+				paste0(names(formals(expect)), collapse = ', '),
+				paste0(deparse(test$args), collapse = ', '))
+		},
+		results)
+	messagef('okay, passed %s tests', length(results))
+}
